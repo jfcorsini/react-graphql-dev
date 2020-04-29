7ef306f50efd292b9be8fad7d8a03098e3c284f1
João Fernando Corsini <jf.corsini@gmail.com>
2020-04-29 09:38:10 +0300

    feat: Allow deleting items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -2,27 +2,48 @@ const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if logged in
 
-    const item = await ctx.db.mutation.createItem({
-      data: {
-        ...args
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
       },
-    }, info);
+      info
+    );
 
     return item;
   },
   updateItem(parent, args, ctx, info) {
     // TODO: Check if logged in
 
-    const updates = { ... args };
+    const updates = { ...args };
     delete updates.id;
 
-    return ctx.db.mutation.updateItem({
-      data: updates,
-      where: {
-        id: args.id,
-      }
-    }, info);
-  }
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = {
+      id: args.id,
+    };
+
+    // Find the item
+    const item = await ctx.db.query.item({ where }, `{ id, title }`);
+
+    // Check if it owns the item, or if it has permission
+    // TODO
+
+    // Delete it
+
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
 };
 
 module.exports = Mutations;
--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
-import { Query } from 'react-apollo'
-import gql from 'graphql-tag'
-import styled from 'styled-components'
-import Item from './Item'
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import styled from "styled-components";
+import Item from "./Item";
 
 const Center = styled.div`
   text-align: center;
@@ -12,7 +12,7 @@ const ItemsList = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-gap: 60px;
-  max-width: ${props => props.theme.maxWidth};
+  max-width: ${(props) => props.theme.maxWidth};
   margin: 0 auto;
 `;
 
@@ -29,22 +29,29 @@ const ALL_ITEMS_QUERY = gql`
   }
 `;
 
-export default class Items extends Component {
+class Items extends Component {
   render() {
     return (
-      <Center >
+      <Center>
         <p>Items!</p>
-        <Query query={ ALL_ITEMS_QUERY }>
+        <Query query={ALL_ITEMS_QUERY}>
           {({ data, error, loading }) => {
-            if (loading) return <p>Loading...</p>
-            if (error) return <p>Error: {error.message}</p>
-            
-            return <ItemsList>
-              {data.items.map(item => <Item item={item} key={item.id}/>)}
-            </ItemsList>
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error: {error.message}</p>;
+
+            return (
+              <ItemsList>
+                {data.items.map((item) => (
+                  <Item item={item} key={item.id} />
+                ))}
+              </ItemsList>
+            );
           }}
         </Query>
       </Center>
-    )
+    );
   }
 }
+
+export default Items;
+export { ALL_ITEMS_QUERY };
