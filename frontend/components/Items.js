d4d4e71f7f418fdd8a616d77a6fd516c8b7ff76a
João Fernando Corsini <jf.corsini@gmail.com>
2020-04-30 10:34:53 +0300

    feat: Add item pagination

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -3,6 +3,8 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import styled from "styled-components";
 import Item from "./Item";
+import Pagination from "./Pagination";
+import { perPage } from "../config";
 
 const Center = styled.div`
   text-align: center;
@@ -17,8 +19,8 @@ const ItemsList = styled.div`
 `;
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -33,8 +35,13 @@ class Items extends Component {
   render() {
     return (
       <Center>
-        <p>Items!</p>
-        <Query query={ALL_ITEMS_QUERY}>
+        <Pagination page={this.props.page} />
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{
+            skip: (this.props.page - 1) * perPage,
+          }}
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error: {error.message}</p>;
@@ -48,6 +55,7 @@ class Items extends Component {
             );
           }}
         </Query>
+        <Pagination page={this.props.page} />
       </Center>
     );
   }
